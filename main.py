61ba97acc9cc56254f99213cddd69741c5dfad76
vdsap <vadim18022004@gmail.com>
2023-06-03 15:16:45 +0300

    moved tg to new file

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
 import asyncio as aio
 from init import main_config, log
-from aiogram import Bot, Dispatcher, Router
-from aiogram.types import Message
-from posters import *
+from tg import tg_bot
 
-    # Load config
-async def main():
+
+async def main():   # Load config
     main_conf = main_config()
-
-
-async def tg():
-    # Start Telegram bot
-    log().debug('Starting Telegram bot')
-    token = main_config()['TG_BOT']['token']
-    router = Router()
-    dp = Dispatcher()
-    bot = Bot(token=token, parse_mode='HTML')
-
-    @router.channel_post()
-    async def get_file(channel_post: Message):
-        log().debug('Getting file')
-        file = channel_post.document
-
-    dp.include_router(router)
-    await dp.start_polling(bot, skip_updates=True)
+    await tg_bot()
 
 
 if __name__ == '__main__':
-    log().info('Script started')
+    log().info('Poster start')
     aio.run(main())
-    aio.run(tg())
